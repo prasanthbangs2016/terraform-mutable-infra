d86e95905decd281e180c4e86c609c7e0a234e51
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-10-11 00:23:51 +0530

    added docdb

--- locals.tf
+++ locals.tf
@@ -15,8 +15,8 @@
 #}
 
 
-locals {
-  apps_subnets_id = module.vpc.subnets["apps"]["out"][*].id
-  db_subnet_ids = module.vpc.subnets["db"]["out"][*].id
-  public_subnet_ids = module.vpc.subnets["public"]["out"][*].id
-}
+#locals {
+#  apps_subnets_id = module.vpc.subnets["apps"]["out"][*].id
+#  db_subnet_ids = module.vpc.subnets["db"]["out"][*].id
+#  public_subnet_ids = module.vpc.subnets["public"]["out"][*].id
+#}
