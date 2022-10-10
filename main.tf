db5dc6c631a0b694f6d3daf9a3d3d353b874d4ad
prasanthbangs2016 <prasanthbangs@gmail.com>
2022-10-11 00:45:41 +0530

    added docdb

--- main.tf
+++ main.tf
@@ -9,18 +9,18 @@ module "vpc" {
     default_route_table_id = var.default_route_table_id
 }
 
-#module "docdb" {
-#    source = "./vendor/modules/docdb"
-#    env     = var.env
-#    engine_version = var.engine_version
-##    apps_subnets_id = local.apps_subnets_id
-##    vpc_id          = module.vpc.vpc_id
-##    vpc_cidr_block  = var.vpc_cidr_block
-##    instance_count  = var.docdb_cluster_instance_count
-#    #docdb_instance_class = var.instance_class
-#
-#
-#}
+module "docdb" {
+    source = "./vendor/modules/docdb"
+    env     = var.env
+    engine_version = var.engine_version
+    apps_subnets_id = local.apps_subnet_ids
+    vpc_id          = module.vpc.vpc_id
+    vpc_cidr_block  = var.vpc_cidr_block
+#    instance_count  = var.docdb_cluster_instance_count
+    #docdb_instance_class = var.instance_class
+
+
+}
 
 
 #output "out" {
@@ -55,6 +55,8 @@ module "vpc" {
 
 locals {
     apps_subnet_ids = module.vpc.out["apps"]["out"][*].id
+    db_subnet_ids = module.vpc.out["db"]["out"][*].id
+    public_subnet_ids = module.vpc.out["public"]["out"][*].id
 }
 
 #output "out" {
